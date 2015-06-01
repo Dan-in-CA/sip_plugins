b95302c02df8e11c236381ac3dbaa83671eb5829
Dan-in-CA <dkimberling@att.net>
2015-06-01 14:35:42 -0700

    Update to show all zones and allow setting enable flags.

--- pulse_cct/pulse_cct.py
+++ pulse_cct/pulse_cct.py
@@ -3,54 +3,79 @@
 to discover the location of a valve'''
 import web
 from time import sleep
-from web import form
 import gv # Get access to ospi's settings
 from urls import urls # Get access to ospi's URLs
-import gpio_pins as gpio
-#urls = ('/pls', 'pulse')
-render = web.template.render('templates/')
+from ospi import template_render  #  Needed for working with web.py templates
+from helpers import stop_stations, jsave
+from webpages import ProtectedPage  # Needed for security
+from gpio_pins import set_output
 
-#app = web.application(urls, globals())
 
-urls.extend(['/pls', 'plugins.pulse_cct.pulse']) 
+urls.extend([
+             '/pls', 'plugins.pulse_cct.pulse',
+             '/pls-run', 'plugins.pulse_cct.p_run',
+             '/pls-stop', 'plugins.pulse_cct.p_stop',
+             '/pls-sen', 'plugins.pulse_cct.p_save_enabled'
+             ]) 
 gv.plugin_menu.append(['Pulse Circuit', '/pls']) # Add this plugin to the home page plugins menu
-gv.curCCT=1 # the curent pulse circuit
-render = web.template.render('templates/')
-pulse_form = form.Form(
-    form.Dropdown('CCT', [1,2,3,4,5,6,7,8]),
-    form.Button("Trigger"),
-    form.Button("Exit")
-)
-pform = pulse_form()
+
+stop = True
+
 def chatter(cct):
-    gv.srvals = [0]*8 #TODO read how many valves are in service, and which ones
-    gpio.set_output() #TODO switch on gpio functions after debugging
-    lv = 1
-    for cnt in range(150):
-        #toggle cct
-        gv.srvals[cct] ^= lv
-        gpio.set_output()
+    stop_stations()
+    t = 0
+    for cnt in range(150):      
+        t = 1 - t #  toggle cct
+        gv.srvals[cct] = t      
+        set_output()
         sleep(0.2)
-    #switch everything off
-    gv.srvals = [0]*8
-    gpio.set_output()
+        if stop:
+            break
+    #  switch everything off
+    stop_stations()
 
-# define what happens when the index page is called
-class pulse:
 
+class pulse(ProtectedPage):
+    """
+    Load an html page for entering plugin settings.
+    """
     def GET(self):
-        pform.CCT.value = gv.curCCT
-#        print 'Rendered, gv.curCCT is %s' %gv.curCCT
-        return render.pulse(pform, "Open Sprinkler")#monthly(levels)
+        return template_render.pulse()  # open settings page
 
 
-    def POST(self):
-        userData = web.input()
-#        print userData
-        if userData.has_key('Exit'):
-            raise web.seeother('/')            
-        else:
-            gv.curCCT = int(userData['CCT'])
-#            print 'Selected Circuit is %s' %gv.curCCT
-            chatter(gv.curCCT-1)
-            raise web.seeother("/pls")
+class p_run():
+    """ Start pulsing selected circuit"""     
+    def GET(self):
+        global stop
+        qdict = web.input()
+ #       print 'qdict: ', qdict
+        stop = False
+        chatter(int(qdict['zone']))
+        raise web.seeother('/pls')
+
+    
+class p_stop():
+    """Stop all pulsing."""  
+    def GET(self):
+        global stop
+        stop = True
+        raise web.seeother('/pls')
+    
+class p_save_enabled():
+    
+    def GET(self):
+        qdict = web.input()
+        print 'qdict: ', qdict
+        for i in range(gv.sd['nbrd']):
+            if 'sh' + str(i) in qdict:
+                try:
+                    gv.sd['show'][i] = int(qdict['sh' + str(i)])
+                except ValueError:
+                    gv.sd['show'][i] = 255
+            if 'd' + str(i) in qdict:
+                try:
+                    gv.sd['show'][i] = ~int(qdict['d' + str(i)])&255
+                except ValueError:
+                    gv.sd['show'][i] = 255
+        jsave(gv.sd, 'sd')
+        raise web.seeother('/')   

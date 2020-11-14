76289d0db7650788956c97ee5bc903a71f856b0c
James <jmsmith86@gmail.com>
2020-11-14 07:20:48 -0700

    Tweaked ssd1306 test components

--- ssd1306/test/execute.bat
+++ ssd1306/test/execute.bat
@@ -1,1 +1,3 @@
+REM Windows regression test execution file.
+REM To run, cd to the test directory, and then execute this file.
 python -B -m pytest -c test.cfg
--- ssd1306/test/execute.sh
+++ ssd1306/test/execute.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-python -B -m pytest -c test.cfg "$@"
+# Linux regression test execution file.
+# To run, cd to the test directory, make this script executable, and then execute this script.
+python -B -m pytest -c test.cfg

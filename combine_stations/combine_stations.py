139f7a61a79ed5c024aa347c18b7be7776cc4cdf
Dan-in-CA <dkimberling@att.net>
2021-08-28 08:27:23 -0700

    Fix bug in display, improve perfoamance.

--- combine_stations/combine_stations.py
+++ combine_stations/combine_stations.py
@@ -7,6 +7,7 @@ from six.moves import range
 
 # standard library imports
 import json  # for working with data files
+from time import sleep
 
 # local module imports
 from blinker import signal  # To receive station notifications
@@ -18,7 +19,7 @@ import web  # web.py framework
 import webpages as wp
 from webpages import ProtectedPage  # Needed for security
 
-skip = 0
+prior_virt = None
 
 # Add new URLs to access classes in this plugin.
 # fmt: off
@@ -56,28 +57,23 @@ def load_settings():
         
 def set_stations(virt):
     """
-    Astivate stations associated with a virtual station.
+    Activate stations associated with a virtual station.
     Selected stations wil run concurrently.
     """
-    global skip #  prevent endless recursion
-
-    if not skip:
-        vid = ((gv.sd['nbrd'] - 1) * 8) + virt
-        print("ready to set stations uaing stations ", com_stations[str(vid)])  # - test
-        for b in range(gv.sd[u"nbrd"] - 1):
-            stn_list = [int(i) - 1 for i in com_stations[str(vid)].split(",")]
-            for sid in stn_list:
-                gv.srvals[sid] = 1 #  used to turn zones on or off (list of one byte per station, 1 = turn on, 0 = turn off)
-                gv.sbits[b] |= 1 << sid #  station bits, used to display stations that are on in UI (list of bytes, one byte per board)
-                gv.ps[sid][0] = 1 #  program schedule used for UI display (list of 2 element lists i.e. [program number, duration])
-                if not gv.sd[u'mm']:
-                    gv.sbits[b] &= 1 << vid #  station bits, used to display stations that are on in UI (list of bytes, one byte per board)
-                    gv.rs[sid][1] =  gv.rs[vid][1]  #  run schedule (list [scheduled start time, scheduled stop time, duration, program number])
-            skip = 1
-            set_output()
-    else:
-        skip = 0
-
+    vid = ((gv.sd[u'nbrd'] - 1) * 8) + virt # vid = key of station group in com_stations dict.
+    stn_list = [int(i) - 1 for i in com_stations[str(vid)].split(",")]  
+    for sid in stn_list:
+        gv.srvals[sid] = 1 #  set gv.srvals on for stations in this group
+    for b in range(gv.sd[u"nbrd"] - 1): #  don't include virtual stations
+        for s in range(8): # for each station per board
+            sidx = b * 8  + s
+            if gv.srvals[sidx]: #  If this station should be on
+                gv.sbits[b] |= 1 << s #  station bits, used to display stations that are on in UI (list of bytes, one byte per board)                
+                gv.ps[sidx][0] = 1
+                if not gv.sd[u'mm']: #  If under program control
+                    gv.ps[sidx][1] = gv.rs[vid][2]                
+                    gv.rs[sidx] =  gv.rs[vid]
+    set_output()
 
 class settings(ProtectedPage):
     """
@@ -104,10 +100,14 @@ class save_settings(ProtectedPage):
     
 ### Control valves ###
 def modify_zone_change(name, **kw):
+    global prior_virt
     if gv.sd[u'seq']: #  if in sequential mode.      
         virtuals = gv.srvals[-8:]
         virt = next((i for i, x in enumerate(virtuals) if x), None)
-        if virt is not None:
+        if (virt is not None
+            and virt != prior_virt
+            ):
+            prior_virt = virt
             set_stations(virt)    
 
 zones = signal(u"zone_change")

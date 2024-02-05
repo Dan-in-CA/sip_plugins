3a125d23262add99073263d648bc0a4d079d7aaa
desouco <desouco@tbdir.net>
2024-02-05 15:47:50 +0000

    Added enable, RUN NOW ignore logic and station parameter logic

--- monthly_adjust_per_station/monthly_adjust_per_station.py
+++ monthly_adjust_per_station/monthly_adjust_per_station.py
@@ -33,11 +33,15 @@ urls.extend([
 gv.plugin_menu.append([_("Monthly Adjust per Station"), "/monthly_adjust_per_station"])
 
 
-ADJUSTMENT_COLORS = ["#e8f4ea", "#e0f0e3", "#d2e7d6", "#c8e1cc", "#b8d8be"]
+# ADJUSTMENT_COLORS = ["#e8f4ea", "#e0f0e3", "#d2e7d6", "#c8e1cc", "#b8d8be"]
+ADJUSTMENT_COLORS = ["#d4ffc5", "#b2eb99", "#87ca6a", "#6bac50", "#54883d"]
 station_settings = {}
 
 
 def validate_int(int_list):
+    """Validate a list of integer strings and return result as a
+    tuple. Invalid integers are returned as None
+    """
     validated_list = []
 
     for index in range(len(int_list)):
@@ -49,52 +53,61 @@ def validate_int(int_list):
     return tuple(validated_list)
 
 
-def notify_stations_scheduled(station, **kw):
-    """todo"""
+def notify_station_scheduled(station, **kw):
+    """Apply the defined adjustment to the schedule running on the
+    station if applicable (Not RUN NOW, enabled)
+    """
 
-    print(station_settings)
-    print(f"=== Before {gv.rs}")
     ts = datetime.datetime.fromtimestamp(gv.now)
     cur_month = ts.month
+    station_index = station - 1
 
-    for station_index in range(0, len(gv.rs)):
-        if gv.rs[station_index][2] > 0:
-            st_mon_key = f"st_mon_{station_index}_{cur_month}"
+    # TODO honor "Ignore Plugin adjustments"
 
-            if st_mon_key in station_settings:
-                (adjustment,) = validate_int(
+    if gv.rn:
+        # Skip RUN NOW schedules
+        return
+
+    st_enable_key = f"enable_{station_index}"
+    st_mon_key = f"st_mon_{station_index}_{cur_month}"
+
+    if st_enable_key not in station_settings:
+        return
+
+    if st_mon_key in station_settings:
+        (adjustment,) = validate_int(
+            [
+                station_settings[st_mon_key],
+            ]
+        )
+
+        if adjustment is None:
+            if "default" not in station_settings:
+                return
+            else:
+                (default,) = validate_int(
                     [
-                        station_settings[st_mon_key],
+                        station_settings["default"],
                     ]
                 )
-                print(adjustment)
+                if default is None or default == 100:
+                    return
+                else:
+                    adjustment = default
 
-                if adjustment is None:
-                    if "default" not in station_settings:
-                        return
-                    else:
-                        (default,) = validate_int(
-                            [
-                                station_settings["default"],
-                            ]
-                        )
-                        if default is None:
-                            return
-                        else:
-                            adjustment = default
+        if adjustment == 100:
+            # Nothing to adjust
+            return
 
-                duration = gv.rs[station_index][2]
+        duration = gv.rs[station_index][2]
 
-                # duration = round(duration * adjustment / 100, 1)
-                # duration = round(duration * adjustment / 100)
-                duration = math.ceil(duration * adjustment / 100)
-                gv.rs[station_index][1] = gv.rs[station_index][0] + duration
-                gv.rs[station_index][2] = duration
-    print(f"=== After {gv.rs}")
+        duration = math.ceil(duration * adjustment / 100)
+        gv.rs[station_index][1] = gv.rs[station_index][0] + duration
+        gv.rs[station_index][2] = duration
 
 
-scheduled_signal = signal("stations_scheduled")
-scheduled_signal.connect(notify_stations_scheduled)
+scheduled_signal = signal("station_scheduled")
+scheduled_signal.connect(notify_station_scheduled)
 
 
 def load_settings():
@@ -106,7 +119,8 @@ def load_settings():
         ) as f:  # Read settings from json file if it exists
             station_settings = json.load(f)
 
-    except IOError:  # If file does not exist return empty value
+    except IOError:
+        # If file does not exist return empty value
         station_settings = {}
 
 
@@ -116,11 +130,9 @@ class get_settings(ProtectedPage):
     """
 
     def GET(self):
-        # load_settings()
 
-        return template_render.monthly_adjust_per_station(
-            station_settings
-        )  # open settings page
+        # open settings page
+        return template_render.monthly_adjust_per_station(station_settings)
 
 
 class save_settings(ProtectedPage):
@@ -130,20 +142,16 @@ class save_settings(ProtectedPage):
     CheckBoxes only appear in qdict if they are checked.
     """
 
-    def GET(self):
+    def set_cell_decoration(self):
+        """Set color of the cells containing adjustments using a
+        gradient scheme."""
+
         global station_settings
+        min_adj = math.inf
+        max_adj = 0
+        adjustments = {}
 
-        # Dictionary of values returned as query string from settings page.
-        qdict = web.input()
-
-        station_settings = qdict
-
-        # Set color of adjustments
-        for st in range(0, len(gv.rs)):
-            min_adj = math.inf
-            max_adj = 0
-            adjustments = {}
-
+        for st in range(0, len(gv.rs)):  # Number of stations
             for mon in range(1, 13):  # 12 months of the year
                 st_mon_key = f"st_mon_{st}_{mon}"
                 (cur_adj,) = validate_int([station_settings[st_mon_key]])
@@ -157,19 +165,29 @@ class save_settings(ProtectedPage):
 
                     adjustments[st_mon_key] = cur_adj
 
-            if len(adjustments) > 0:
-                old_range = max_adj - min_adj
+        if len(adjustments) > 0:
+            old_range = max_adj - min_adj
 
-                for st_mon_key in adjustments.keys():
-                    cur_adj = adjustments[st_mon_key]
+            for st_mon_key in adjustments.keys():
+                cur_adj = adjustments[st_mon_key]
 
-                    if old_range == 0:
-                        color_idx = 0
-                    else:
-                        # Distribute adjustments evenly in range 0-4
-                        color_idx = math.trunc((cur_adj - min_adj) * 4.9 / old_range)
+                if old_range == 0:
+                    color_idx = 0
+                else:
+                    # Distribute adjustments evenly in range 0-4
+                    color_idx = math.trunc((cur_adj - min_adj) * 4.9 / old_range)
 
-                    station_settings[f"c_{st_mon_key}"] = ADJUSTMENT_COLORS[color_idx]
+                station_settings[f"c_{st_mon_key}"] = ADJUSTMENT_COLORS[color_idx]
+
+    def GET(self):
+        global station_settings
+
+        # Dictionary of values returned as query string from settings page.
+        qdict = web.input()
+
+        station_settings = qdict
+
+        save_settings.set_cell_decoration(self)
 
         with open("./data/monthly_adjust_per_station.json", "w") as f:
             json.dump(station_settings, f)

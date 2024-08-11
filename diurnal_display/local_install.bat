f6fd7e42392585446aabcdff85d5fb3c71f745d5
Jonathan Marsh <jonathan@wso2.com>
2024-08-11 15:27:35 -0700

    remove dev scripts

--- diurnal_display/local_install.bat
+++
@@ -1,5 +0,0 @@
-xcopy diurnal_display.py ..\..\SIP\plugins /y
-xcopy diurnal_display.html ..\..\SIP\templates /y
-xcopy diurnal_display.js ..\..\SIP\static\scripts /y
-xcopy diurnal_display-docs.html ..\..\SIP\static\docs\plugins /y
-xcopy diurnal_display.manifest ..\..\SIP\plugins\manifests /y
--- diurnal_display/local_install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-cp -f diurnal_display.py ../../SIP/plugins 
-cp -f diurnal_display.html ../../SIP/templates 
-cp -f diurnal_display.js ../../SIP/static/scripts
-cp -f diurnal_display-docs.html ../../SIP/static/docs/plugins
-cp -f diurnal_display.manifest ../../SIP/plugins/manifests

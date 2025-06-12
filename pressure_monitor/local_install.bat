7414f3dc04d79e9c5b102021c9154c7de0b5d0ed
Jonathan Marsh <jonathan@wso2.com>
2025-06-12 13:29:32 -0700

    Revert "Custom pressure monitor plugin"
    This reverts commit 54dc832a11f0ed6a36e4db21870a561c9f286ff7.

--- pressure_monitor/local_install.bat
+++
@@ -1,5 +0,0 @@
-xcopy pressure_monitor.pyl ..\..\SIP\plugins /y
-xcopy pressure_monitor.html ..\..\SIP\templates /y
-xcopy pressure_monitor.js ..\..\SIP\static\scripts /y
-xcopy pressure_monitor-docs.html ..\..\SIP\static\docs\plugins /y
-xcopy pressure_monitor.manifest ..\..\SIP\plugins\manifests /y
